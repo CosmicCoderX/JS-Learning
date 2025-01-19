00ae89469e3c6a18288f6e86cffdc727459410e8
Suryakant Dwivedi <147908822+CosmicCoderX@users.noreply.github.com>
2025-01-19 12:48:13 +0000

    new 03_conversionOperations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -13,7 +13,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = "Surya"
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
+console.log(booleanisLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -21,5 +21,42 @@ console.log(booleanisLoggedIn)
 
 let someNumber = 89
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+/*********************************************Operation******************************************************************* */
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Surya"
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log((5 + 4) * 9 % 3);
+
+// console.log(true+); it gives an error
+console.log(+true); // 1
+console.log(+""); // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 
+
+let gameCounter = 100
+gameCounter++
+++gameCounter // prefix
+console.log(gameCounter)
