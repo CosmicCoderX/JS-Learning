e5ae2e3155e6328061f742d938eb1b21c909f987
Suryakant Dwivedi <147908822+CosmicCoderX@users.noreply.github.com>
2025-01-19 12:06:15 +0000

    03_conversionOperation

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = null
+
+console.log(typeof (score)); //parenthesis ke andar jo variable hai voh ab method ban chuka hai
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abs" => NaN (not a number)
+// true => 1; false => 0
+// null => 0; undefined => NaN
+
+let isLoggedIn = "Surya"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Surya" => true
+
+let someNumber = 89
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
