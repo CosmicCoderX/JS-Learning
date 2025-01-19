82c5fb4facde47d64d9742f82049c4898626ce9c
Suryakant Dwivedi <147908822+CosmicCoderX@users.noreply.github.com>
2025-01-19 13:08:42 +0000

    04_comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(1 != 3);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log( null == 0);
+console.log(null >= 0); // true (becoz comparision converts null to 0)
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+
+// === (stict check here value is checked first then datatype is also checked that they are equal or not)
+console.log("2" === 2);
+
